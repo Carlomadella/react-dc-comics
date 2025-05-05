ebcd1ebbe0fbff8b9a200e594a347c1aa6ad1d37
Carlomadella <madella871@gmail.com>
2025-05-05 22:13:08 +0200

    [UPD] finito footer, mancano ultimi aggiustamenti

---
+++ react-dc-comics/src/components/Footer.jsx
@@ -0,0 +1,72 @@
+const Footer = () => {
+    return (
+        <footer>
+        <div className="container">
+            <div className="firstSection">
+                <div className="firstUl">
+                    <ul>
+                        <h4>DC COMICS</h4>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Collectibles</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">Fans</a></li>
+                        <li><a href="#">News</a></li>
+                        <h4>SHOP</h4>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC collectibles</a></li>
+                    </ul>
+                </div>
+                <div className="secondUl">
+                    <ul>
+                        <h4>DC </h4>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                    </ul>
+                </div>
+                <div className="thirdUl">
+                    <ul>
+                        <h4>SITES</h4>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                        <li><a href="#">Lorem</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="secondSection">
+                <div>
+                    <button>SIGN-UP NOW!</button>
+                </div>
+                <div className="social">
+                    <div><h4>FOLLOW US</h4></div>
+                    <div>
+                    <ul>
+                        <li><a href="#"><img src="../src/assets/img/footer-facebook.png" alt="facebook" /></a></li>
+                        <li><a href="#"><img src="../src/assets/img/footer-twitter.png" alt="twitter" /></a></li>
+                        <li><a href="#"><img src="../src/assets/img/footer-youtube.png" alt="youtube" /></a></li>
+                        <li><a href="#"><img src="../src/assets/img/footer-pinterest.png" alt="pinterest" /></a></li>
+                        <li><a href="#"><img src="../src/assets/img/footer-periscope.png" alt="periscope" /></a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
