8b753875429cfc7562362f34d5f046c7a2abcbc2
Carlomadella <madella871@gmail.com>
2025-05-05 19:21:36 +0200

    [UPD] creata cartella component

---
+++ react-dc-comics/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <img src="/logo.png" alt="My Website" />
+                </div>
+            </div>
+        </header>
+    </>
+  );
+}
+
+export default Header;
