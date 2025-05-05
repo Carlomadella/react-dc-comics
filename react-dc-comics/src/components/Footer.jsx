1d44e112d6278099cc749ef6890b31df7cbab76f
Carlomadella <madella871@gmail.com>
2025-05-06 01:41:48 +0200

    [UPD] aggiustamenti main e footer

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,54 +1,59 @@
 const Footer = () => {
     return (
         <footer>
-        <div className="container">
-            <div className="firstSection">
-                <div className="firstUl">
-                    <ul>
-                        <h4>DC COMICS</h4>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Collectibles</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">Fans</a></li>
-                        <li><a href="#">News</a></li>
-                        <h4>SHOP</h4>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC collectibles</a></li>
-                    </ul>
-                </div>
-                <div className="secondUl">
-                    <ul>
-                        <h4>DC </h4>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                    </ul>
-                </div>
-                <div className="thirdUl">
-                    <ul>
-                        <h4>SITES</h4>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                        <li><a href="#">Lorem</a></li>
-                    </ul>
+        
+            <div className="top-footer">
+                <div className="container">
+                    <div className="footerList">
+                        <div className="firstUl">
+                            <ul>
+                                <h4>DC COMICS</h4>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Collectibles</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">Fans</a></li>
+                                <li><a href="#">News</a></li>
+                                <h4>SHOP</h4>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC collectibles</a></li>
+                            </ul>
+                        </div>
+                        
+                        <div className="secondUl">
+                            <ul>
+                                <h4>DC </h4>
+                                <li><a href="#">Therms of use</a></li>
+                                <li><a href="#">Privacy policy (New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings Shop</a></li>
+                                <li><a href="#">Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="thirdUl">
+                            <ul>
+                                <h4>SITES</h4>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="secondSection">
-                <div>
+            <div className="bot-footer">
+                <div className="button">
                     <button>SIGN-UP NOW!</button>
                 </div>
                 <div className="social">
@@ -64,7 +69,6 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-        </div>
     </footer>
     )
 }
--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -2,48 +2,48 @@ const Main = () => {
   return (
     <main>
             {/* prima sezione */}
-            <div className="firstSection">
+            <div className="top-footer">
                 <div className="container">
                     <h2>Content goes here</h2>
                 </div>
             </div>
 
             {/* seconda sezione */}
-            <div className="secondSection">
+            <div className="bot-footer">
                 <div className="container">
                     <div className="cards">
                         {/* prima card */}
                         <div className="card">
                             <figure>
-                                <img src="../src/assets/img/buy-comics-digital-comics"  />
+                                <img src="../src/assets/img/buy-comics-digital-comics.png"  />
                             </figure>
                             <h6>DIGITAL COMICS</h6>
                         </div>
                         {/* seconda card */}
                         <div className="card">
                             <figure>
-                                <img src="../src/assets/img/buy-comics-digital-comics"  />
+                                <img src="../src/assets/img/buy-comics-merchandise.png"  />
                             </figure>
                             <h6>DC MERCHANDISE</h6>
                         </div>
                         {/* terza card */}
                         <div className="card">
                             <figure>
-                                <img src="../src/assets/img/buy-comics-digital-comics"  />
+                                <img src="../src/assets/img/buy-comics-subscriptions.png"  />
                             </figure>
                             <h6>SUBSCRIPTION</h6>
                         </div>
                         {/* quarta card */}
                         <div className="card">
                             <figure>
-                                <img src="../src/assets/img/buy-comics-digital-comics"  />
+                                <img src="../src/assets/img/buy-comics-shop-locator.png"  />
                             </figure>
                             <h6>COMIC SHOP LOCATOR</h6>
                         </div>
                         {/* quinta card */}
                         <div className="card">
                             <figure>
-                                <img src="../src/assets/img/buy-comics-digital-comics"  />
+                                <img src="../src/assets/img/buy-dc-power-visa.svg"  />
                             </figure>
                             <h6>DC POWER VISA</h6>
                         </div>
