4d75827d9a2fb6317ac8a255ccdd6a14b567fbd3
Carlomadella <madella871@gmail.com>
2025-05-06 18:11:59 +0200

    [UPD] completato punto 5 dell'esercizio

--- react-dc-comics/src/components/ListComics.jsx
+++ react-dc-comics/src/components/ListComics.jsx
@@ -5,7 +5,7 @@ const ListComics = () => {
     return (
         <div id="fumetti">
             <div className="comics-container">
-                
+                <ComicCard src={comics[0].thumb} title={comics[0].series}/>
             </div>
         </div>
     )
