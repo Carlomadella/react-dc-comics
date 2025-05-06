14728180dc968f1fee77f5f469df471b8d3a0677
Carlomadella <madella871@gmail.com>
2025-05-06 18:12:23 +0200

    [UPD] completato esercizio

--- react-dc-comics/src/components/ListComics.jsx
+++ react-dc-comics/src/components/ListComics.jsx
@@ -6,6 +6,17 @@ const ListComics = () => {
         <div id="fumetti">
             <div className="comics-container">
                 <ComicCard src={comics[0].thumb} title={comics[0].series}/>
+                <ComicCard src={comics[1].thumb} title={comics[1].series}/>
+                <ComicCard src={comics[2].thumb} title={comics[2].series}/>
+                <ComicCard src={comics[3].thumb} title={comics[3].series}/>
+                <ComicCard src={comics[4].thumb} title={comics[4].series}/>
+                <ComicCard src={comics[5].thumb} title={comics[5].series}/>
+                <ComicCard src={comics[6].thumb} title={comics[6].series}/>
+                <ComicCard src={comics[7].thumb} title={comics[7].series}/>
+                <ComicCard src={comics[8].thumb} title={comics[8].series}/>
+                <ComicCard src={comics[9].thumb} title={comics[9].series}/>
+                <ComicCard src={comics[10].thumb} title={comics[10].series}/>
+                <ComicCard src={comics[11].thumb} title={comics[11].series}/>
             </div>
         </div>
     )
